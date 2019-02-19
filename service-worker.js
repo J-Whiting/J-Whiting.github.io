475abecd238d992ee6258fb9490ba8b2768b7977
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-02-19 14:14:16 +0000

    Updating Service Worker
    Removing the offline website, and instead caching all the files

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,85 @@
-self.addEventListener('install', function(e) {
+const version = "1.0";
+const cacheName = "jw-${version}";
+
+self.addEventListener("install", e => {
 	e.waitUntil(
-		caches.open('jw').then(function(cache) {
+		caches.open(cacheName).then(cache => {
 			return cache.addAll([
-				'offline.html',
-				'manifest.json',
-				'css/font.css',
-				/* Roboto */
-				'https://fonts.googleapis.com/css?family=Roboto',
-				'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2',
-				'images/logo/logo-192.png'
+				"/",
+				"/index.html",
+				"/Joshua-Whiting-CV.pdf",
+				"/manifest.json",
+				"/css/button.css",
+				"/css/card.css",
+				"/css/color.css",
+				"/css/elevation.css",
+				"/css/font.css",
+				"/css/header.css",
+				"/css/main.css",
+				"/css/reset.css",
+				"https://fonts.googleapis.com/css?family=Roboto",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu72xKOzY.woff2",
+				"https://fonts.googleapis.com/icon?family=Material+Icons",
+				"https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+				"images/education/background.jpg",
+				"images/education/hull-media.jpg",
+				"images/education/hull-thumbnail.jpg",
+				"images/education/nottingham-media.jpg",
+				"images/education/nottingham-thumbnail.jpg",
+				"images/experience/background.jpg",
+				"images/hobbies/background.jpg",
+				"images/hobbies/language-media.jpg",
+				"images/hobbies/language-thumbnail.jpg",
+				"images/hobbies/music-media.jpg",
+				"images/hobbies/music-thumbnail.jpg",
+				"images/hobbies/sport-media.jpg",
+				"images/hobbies/sport-thumbnail.jpg",
+				"images/home/background.jpg",
+				"images/home/media.jpg",
+				"images/home/thumbnail.jpg",
+				"images/logo/logo-192.png",
+				"images/logo/logo-192.svg",
+				"images/logo/logo-512.png",
+				"images/logo/logo-512.svg",
+				"js/main.js"
+			])
+			.then(() => self.skipWaiting());
+		})
+	);
+});
+
+self.addEventListener("activate", event => {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName)
+		.then(cache => cache.match(event.request, {ignoreSearch: true}))
+		.then(response => {
+			return response || fetch(event.request);
+		})
+	);
+});
+
+
+
+/*self.addEventListener("install", function(e) {
+	e.waitUntil(
+		caches.open("cacheName").then(function(cache) {
+			return cache.addAll([
+				"offline.html",
+				"manifest.json",
+				"css/font.css",
+				"https://fonts.googleapis.com/css?family=Roboto",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2",
+				"images/logo/logo-192.png"
 			]);
 		})
 	);
 });
 
-/*self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
 	console.log(event.request.url);
 	
 	event.respondWith(
@@ -22,16 +87,16 @@ self.addEventListener('install', function(e) {
 			return response || fetch(event.request);
 		})
 	);
-});*/
+});
 
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
+this.addEventListener("fetch", event => {
+  // request.mode = navigate isn"t supported in all browsers
   // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+  if (event.request.mode === "navigate" || (event.request.method === "GET" && event.request.headers.get("accept").includes("text/html"))) {
         event.respondWith(
           fetch(event.request.url).catch(error => {
               // Return the offline page
-              return caches.match('offline.html');
+              return caches.match("offline.html");
           })
     );
   }
@@ -43,4 +108,4 @@ this.addEventListener('fetch', event => {
                     })
             );
       }
-});
+});*/
