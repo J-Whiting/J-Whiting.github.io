83e41f71b4038a81e6f4f600ed3233b5c41c59d3
Joshua Bee <josh.bee@strawberry.co.uk>
2020-09-17 21:39:22 +0100

    Update
    Adding BEM Rules
    Adding ITCSS Rules
    Moving icons locally
    Update JS

--- js/main.js
+++ js/main.js
@@ -3,68 +3,98 @@ let vSnackbarOpen = true;
 let vInstalled = true;
 let vStandalone = false;
 
-document.addEventListener('DOMContentLoaded', function(event){
-	//
-	// Updating selected nav tab based on scroll position.
-	//
-	vNavItems = document.getElementById("headerNavigation").children[0].children;
-	vActiveID = -1;
-	
-	document.getElementById("install-close").addEventListener('click', event => {
+document.addEventListener('DOMContentLoaded', (event) => {	
+	document.getElementById('snackbar-close').addEventListener('click', (event) => {
 		vSnackbarOpen = false;
-		document.getElementById("install-snackbar").style.bottom = '-5em';
+		document.getElementById('snackbar').classList.remove('snackbar--active');
 	});
 	
 	if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone) {
 		console.log('display-mode is standalone');
 		vStandalone = true;
 	}
-	
-	window.onscroll = function() {
+
+	// Add scroll link events
+	let scrollElements = document.querySelectorAll('[data-scroll-to]');
+	scrollElements.forEach((element) => {
+		element.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			let scrollElement = document.getElementById(event.currentTarget.dataset.scrollTo);
+			let header = document.querySelector('header');
+
+			window.scrollTo({
+				top: scrollElement.offsetTop - header.offsetHeight,
+				behavior: 'smooth'
+			});
+		});
+	});
+
+	// Add Card events
+	let cards = document.querySelectorAll('[data-cards]');
+	cards.forEach((card) => {
+		card.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			let card = event.currentTarget;
+			let cards = document.querySelectorAll(`[data-cards="${ card.dataset.cards }"`);
+
+			cards.forEach((card) => {
+				card.classList.remove('card--selected');
+			});
+			card.classList.add('card--selected');
+		});
+	});
+
+	//
+	// Updating selected nav tab based on scroll position.
+	//
+	vNavItems = document.querySelectorAll('.navigation__item');
+	vActiveID = -1;
+	window.addEventListener('scroll', (event) => {
 		// Open snackbar upon scroll
 		if (!vInstalled && !vStandalone && vSnackbarOpen) {
-			// Update UI notify the user they can add to home screen
-			document.getElementById("install-snackbar").style.bottom = '1em';
+			document.getElementById('snackbar').classList.add('snackbar--active');
 		}
 		
-		var doc = document.documentElement;
-		var vScroll = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+		let doc = document.documentElement;
+		let vScroll = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 
 		// Get the height of the browser.
-		var g = document.getElementsByTagName('body')[0];
-		var vBrowserHeight = window.innerHeight || doc.clientHeight || g.clientHeight;
+		let g = document.getElementsByTagName('body')[0];
+		let vBrowserHeight = window.innerHeight || doc.clientHeight || g.clientHeight;
 
 		// Get the height of the header.
-		var vHeader = document.getElementsByTagName('header')[0];
-		var vHeaderHeight = vHeader.clientHeight;
+		let vHeader = document.getElementsByTagName('header')[0];
+		let vHeaderHeight = vHeader.clientHeight;
 
 		// Get the sheet height.
-		var vSheetHeight = vBrowserHeight - vHeaderHeight;
-		var vNavID = Math.floor(vScroll / vSheetHeight);
+		let vSheetHeight = vBrowserHeight - vHeaderHeight;
+		let vNavID = Math.floor(vScroll / vSheetHeight);
 		vNavID--;
-
+	
 		// Only update the classes if they change.
 		if (vNavID != vActiveID) {
-			var vNavItem;
-			for (var i = 0; i < vNavItems.length; i++) {
+			let vNavItem;
+			for (let i = 0; i < vNavItems.length; i++) {
 
 				vNavItem = vNavItems[i];
 				if (i == vNavID) {
-					vNavItem.classList.add("selected");
+					vNavItem.classList.add('navigation__item--selected');
 				}
 				else {
-					vNavItem.classList.remove("selected");
+					vNavItem.classList.remove('navigation__item--selected');
 				}
 			}
 
 			// Update the active tab id.
 			vActiveID = vNavID;
 		}
-	};
+	});
 });
 
 // https://developers.google.com/web/ilt/pwa/lab-offline-quickstart#52_activating_the_install_prompt
-window.addEventListener('beforeinstallprompt', event => {
+window.addEventListener('beforeinstallprompt', (event) => {
 	
 	// The application is not installed
 	vInstalled = false;
@@ -76,7 +106,7 @@ window.addEventListener('beforeinstallprompt', event => {
 	vDeferredPrompt = event;
 
 	// Attach the install prompt to a user gesture
-	document.getElementById("install-action").addEventListener('click', event => {
+	document.getElementById('snackbar-action').addEventListener('click', (event) => {
 
 		// Show the prompt
 		vDeferredPrompt.prompt();
@@ -95,65 +125,9 @@ window.addEventListener('beforeinstallprompt', event => {
 });
 
 // When the app is installed it should remove the install snackbar
-window.addEventListener('appinstalled', (evt) => {
+window.addEventListener('appinstalled', (event) => {
 	console.log('a2hs installed');
 	vInstalled = true;
 	vStandalone = true;
-	document.getElementById("install-snackbar").style.bottom = '-5em';
+	document.getElementById('snackbar').classList.remove('snackbar--active');
 });
-
-function pageScroll(id) {
-	var vEl = document.getElementById(id);
-	var vHeader = document.getElementById("header");
-
-	// Get the desired scroll position
-	var desiredScrollPosition = vEl.offsetTop - vHeader.offsetHeight;
-
-	var isIE = !!document.documentMode;
-	var isEdge = !!/Edge\//.test(navigator.userAgent);
-
-	// Scroll.
-	if (isIE || isEdge) {
-		window.scroll(0, desiredScrollPosition);
-	}
-	else {
-		window.scrollTo({
-			top: desiredScrollPosition,
-			behavior: "smooth"
-		});
-	}
-}
-
-//
-// Functions to apply hover to cards.
-//
-function cardMouseOver(el) {
-	if (!el.classList.contains("selected")) {
-		el.classList.remove("elevation-1dp");
-		el.classList.add("elevation-8dp");
-	}
-}
-
-function cardMouseOut(el) {
-	if (!el.classList.contains("selected")) {
-		el.classList.remove("elevation-8dp");
-		el.classList.add("elevation-1dp");
-	}
-}
-
-function selectCard(el) {
-	// Remove selected class from the card-list.
-	var vEls = el.parentNode.childNodes;
-	for (var i = 0; i < vEls.length; i++) {
-		if (vEls[i].nodeType == 1) {
-			vEls[i].classList.remove("selected");
-			vEls[i].classList.remove("elevation-8dp");
-			vEls[i].classList.add("elevation-1dp");
-		}
-	}
-
-	// Add selected class.
-	el.classList.remove("elevation-1dp");
-	el.classList.add("selected");
-	el.classList.add("elevation-8dp");
-}
